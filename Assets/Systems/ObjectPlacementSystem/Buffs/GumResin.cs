e9c7d47a1022ca7cd22fa3c6cca7f88e79521d22
Sanyam Mahajan <sanyammahajan09@gmail.com>
2025-12-25 10:32:44 +0530

    <Fix Gum resin>

--- Assets/Systems/ObjectPlacementSystem/Buffs/GumResin.cs
+++ Assets/Systems/ObjectPlacementSystem/Buffs/GumResin.cs
@@ -1,27 +1,44 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GumResin : BuffEffect
 {
-    [SerializeField] private GameObject gumResinPrefab; 
     public override void Init()
     {
-        if(TryGetComponent(out AttackComponent attackComponent))
+        if (TryGetComponent(out AttackComponent attackComponent))
         {
-            attackComponent.projectileModel = gumResinPrefab;
-            attackComponent.AdditionEffectOnTarget = OnAdditionalEffectOnTargetOnCollision;
+            if (AnimationPool.Instance != null)
+            {
+                AnimationPool.Instance.Play_CFXR2_Shiny_Item_Loop__AnimationAtFor(transform.forward, transform.position, 0.7f);
+            }
+            attackComponent.AdditionEffectOnTarget += OnAdditionalEffectOnTargetOnCollision;
         }
     }
-
+    private void OnDestroy()
+    {
+        if (TryGetComponent(out AttackComponent attackComponent))
+        {
+            attackComponent.AdditionEffectOnTarget -= OnAdditionalEffectOnTargetOnCollision;
+        }
+    }
 
     private void OnAdditionalEffectOnTargetOnCollision(Transform target)
     {
-        if(target != null && target.TryGetComponent(out StatsComponent stats) && stats.FactionType!= GetComponent<StatsComponent>().FactionType)
+        if (target != null && target.TryGetComponent(out StatsComponent stats) && stats.FactionType != GetComponent<StatsComponent>().FactionType)
         {
-            stats.SpeedMultiplier = 0.5f;
-            AnimationPool.Instance.Play_CFXR_Water_Splash_Smaller__AnimationAtFor(target.forward,target.position,2f);
+            var hits = Physics.OverlapSphere(target.position, 1f);
+
+            foreach (var hit in hits)
+            {
+                if (hit.TryGetComponent(out StatsComponent hitStats) && hitStats.FactionType != GetComponent<StatsComponent>().FactionType)
+                {
+                    hitStats.SpeedMultiplier = 0.5f;
+                }
+            }
+            AnimationPool.Instance.Play_CFXR_Water_Splash_Smaller__AnimationAtFor(target.forward, target.position, 2f);
             Debug.Log("[Gum Resin]");
         }
     }
-    
+
 
 }
