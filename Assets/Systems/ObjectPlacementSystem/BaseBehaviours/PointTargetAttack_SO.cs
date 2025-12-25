a604dcbe2c29693e8b98624b4c5d5b377b0c6238
Sanyam Mahajan <sanyammahajan09@gmail.com>
2025-12-25 10:36:44 +0530

    <unsaved changes from previous commit>

--- Assets/Systems/ObjectPlacementSystem/BaseBehaviours/PointTargetAttack_SO.cs
+++ Assets/Systems/ObjectPlacementSystem/BaseBehaviours/PointTargetAttack_SO.cs
@@ -6,11 +6,11 @@ public class PointTargetAttack_SO : AttackBehaviour_SO
 {
     override public void ExecuteBehaviour(AttackComponent attackComponent, Transform AttackPoint, Transform Target)
     {
-        
+
         if (ProjectilePool.Instance != null)
         {
             var projectile = ProjectilePool.Instance.GetProjectile();
-            projectile.Init(Faction.GoodGuys, AttackPoint.position, Target.position, damage: BaseDamage, lerpFunc: ProjectileBehaviorScript.LerpFunc,onTriggerEnterCallBack:OnTriggerEnterCall, firedFrom: attackComponent,attackComponent.projectileModel);
+            projectile.Init(Faction.GoodGuys, AttackPoint.position, Target.position, damage: BaseDamage, lerpFunc: ProjectileBehaviorScript.LerpFunc, onTriggerEnterCallBack: OnTriggerEnterCall, firedFrom: attackComponent, attackComponent.projectileModel);
             Debug.Log(name + "<color=purple> Executed Point Attack Behaviour !</color>");
         }
     }
@@ -25,7 +25,7 @@ public class PointTargetAttack_SO : AttackBehaviour_SO
         {
             targetHealth.DeductHealth(projectile.Damage);
             projectile.Deactivate();
-            if(projectile.FiredFromAttackComponent!=null && projectile.FiredFromAttackComponent.AdditionEffectOnTarget != null)
+            if (projectile.FiredFromAttackComponent != null && projectile.FiredFromAttackComponent.AdditionEffectOnTarget != null)
             {
                 projectile.FiredFromAttackComponent.AdditionEffectOnTarget(targetHealth.transform);
             }
